5f59c22cdbd2a2ce114266f53bc346da19e5af16
Sihyun <sihyun930222@gmail.com>
2021-11-21 16:04:01 +0900

    chore: webpack 설정 babel-loader 적용

--- webpack.common.js
+++ webpack.common.js
@@ -5,13 +5,20 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
-  entry: './src/main.js', // entry 파일 지정
+  entry: './src/index.js', // entry 파일 지정
   output: {
     filename: 'main.js', // 빌드 후 생성될 파일 이름
     path: path.resolve(__dirname, 'dist'), // 빌드 파일이 생성 될 경로
   },
   module: {
     rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
       {
         // css 파일에 로더 적용
         test: /\.css$/, // 확장자가 css파일들에 적용
@@ -36,7 +43,7 @@ module.exports = {
   plugins: [
     //  html 파일 모듈화를 위해 플러그인 적용
     new HtmlWebpackPlugin({
-      template: './index.html',
+      template: './public/index.html',
     }),
     //  빌드 과정에서 css 파일 따로 추출하기 위해 CssExtractPlugin 적용
     new MiniCssExtractPlugin({
