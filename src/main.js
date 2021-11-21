baa46996080ecfa74848ce2bf822bfe902ad33d8
Sihyun <sihyun930222@gmail.com>
2021-11-21 23:57:44 +0900

    refactor :  미사용 파일 삭제

--- src/main.js
+++
@@ -1,7 +0,0 @@
-import { makeSentence, makeAvatar } from './utils';
-import './style.css';
-
-const result = makeSentence('seeh_h');
-const root = document.querySelector('#root');
-
-makeAvatar(root, result);
